e1eea0e1e1cb84076ba73616c60c936d56eb1efc
Aldarund <shaper.int@gmail.com>
2018-01-18 16:49:23 +0300

    signature field typed & text version

--- src/fields/core/fieldSignature.vue
+++ src/fields/core/fieldSignature.vue
@@ -1,6 +1,46 @@
 <template>
-     <vueSignature ref="signature"/>
+    <div>
+        <div v-if="mode === 'graph'">
+            <vueSignature ref="signature"/>
+            <a @click="mode = 'text'">Prefer to type your signature? Click here</a>
+
+            <v-btn @click="save()">save</v-btn>
+            <v-btn @click="clear()">clear</v-btn>
+        </div>
+        <div v-else="mode === 'text'">
+            <div class="signature-text">{{localValue}}</div>
+            <v-text-field
+                    v-model="localValue"
+                    :label="field.label"
+                    :required="field.required"
+                    :readonly="field.editable"
+                    :disabled="field.disabled"
+                    :placeholder="field.placeholder"
+                    :name="field.name"
+                    :id="field.name"
+                    @blur="onBlur"
+                    @change="onChange"
+                    @focus="onFocus"
+                    @input="onInput"
+            />
+            <a @click="mode = 'graph'">Prefer to draw your signature? Click here</a>
+        </div>
+    </div>
 </template>
+<style scoped>
+@font-face {
+    font-family: 'Quentin';
+    src: url('./../../assets/fonts/Quentin.woff2') format('woff2'),
+        url('./../../assets/fonts/Quentin.woff') format('woff');
+    font-weight: bold;
+    font-style: normal;
+}
+.signature-text {
+    font-family: Quentin, sans-serif;
+    font-size: 30px;
+}
+</style>
+
 <script>
   import abstractField from "../abstractField"
   import VueSignature from 'vue-signature/src/lib/vue-signature'
@@ -13,9 +53,18 @@
     },
     data () {
       return {
+        mode: 'graph'
       }
     },
     methods: {
+      save () {
+        let sign = this.$refs.signature.save()
+        this.$emit('upd', sign, this.field.name)
+      },
+      clear () {
+        this.$refs.signature.clear()
+        this.$emit('upd', '', this.field.name)
+      }
     }
   }
 </script>
